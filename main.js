ef72e2f423deae85fb4e775998b998c873d28e9c
MVNirmal <81420492+MVNirmal@users.noreply.github.com>
2021-06-28 17:24:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name ="Ranbir";
+var last_name ="Kapoor";
+
+console.log(first_name);
+console.log(last_name);
+
+var a ="10";
+var b = "5";
+var add = 10 + 5;
+var sub = a - b;
+var mul = a * b;
+var div = a / b;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
